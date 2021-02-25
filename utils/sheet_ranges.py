b4b93c8bc2e363b60aa4c0f2f0c30c3639b26a83
connordunham <37425411+connordunham@users.noreply.github.com>
2021-02-25 16:55:45 -0500

    Major gsheet range update.

--- utils/sheet_ranges.py
+++ utils/sheet_ranges.py
@@ -1,8 +1,51 @@
 
 SHEET_RANGES = {
-    "Portfolio_Holdings":{
+    "Portfolio Holdings":{
         "CURRENT_HOLDINGS":"'Portfolio Holdings'!B1:J10",
         "DATE_LAST_MODIFIED":"'Portfolio Holdings'!A1"
 
+    },
+    "Historic":{
+        "DAILY_WRITE": "'Holdings'!A4:F12"
+        "DAILY_READ": "'Holdings'!A3:F11"
+    },
+    "Growth & Metrics":{
+        "DAILY_CHANGE": "'Growth & Metrics'!B3",
+        "DAILY_PERCENTAGE": "'Growth & Metrics'!B4",
+        "MARKET": "'Growth & Metrics'!B5",
+        "GAIN_ON_MARKET": "'Growth & Metrics'!B6"
+        "SHARPE_RATIO": "'Growth & Metrics'!B7"
+    },
+    "Misc_Values":{
+        "TODAY": "'DONOTTOUCH'!B2",
+        "FISRT_DAY": "'DONOTTOUCH'!B3",
+        "YEAR": "'DONOTTOUCH'!B4",
+        "WEEK_FLOAT": "'DONOTTOUCH'!B5",
+        "WEEK_INT": "'DONOTTOUCH'!B6",
+        "REF_2021": "'DONOTTOUCH'!B7",
+        "REF_2022": "'DONOTTOUCH'!B8",
+        "REF_2023": "'DONOTTOUCH'!B9"
+    },
+    "Commodities": {
+        "GOLD":{
+            "DAILY":{
+                "OPEN":"'DONOTTOUCH'!B13",
+                "CLOSE":"'DONOTTOUCH'!C13"
+            },
+            "WEEKLY":{
+                "OPEN":"'DONOTTOUCH'!B14",
+                "CLOSE":"'DONOTTOUCH'!C14"
+            }
+        },
+        "SILVER":{
+            "DAILY":{
+                "OPEN":"'DONOTTOUCH'!B16",
+                "CLOSE":"'DONOTTOUCH'!C16"
+            },
+            "WEEKLY":{
+                "OPEN":"'DONOTTOUCH'!B17",
+                "CLOSE":"'DONOTTOUCH'!C17"
+            }
+        }
     }
 }
